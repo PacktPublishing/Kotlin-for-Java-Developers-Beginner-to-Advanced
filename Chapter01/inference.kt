ed2ae2cdffc554d4de953856ac3bd48c5fcdbad0
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-07-04 00:37:42 -0500

    Inference example

---
+++ Chapter01/inference.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //The developer define the type
+    //var name: String = "Noemi"
+
+    //Kotlin use the inference because the developer doesnt assign a type
+    var name = "Noemi"
+
+}
