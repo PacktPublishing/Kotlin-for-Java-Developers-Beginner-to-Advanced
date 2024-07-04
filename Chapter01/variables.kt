4748eaaad253539ef31a1c96cc6621da54fa5604
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-07-04 00:32:26 -0500

    Variables code val-var

---
+++ Chapter01/variables.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    //We can change the value if we use var
+    /* var name: String = "Jose"
+    name = "Dimas" */
+
+    //We can't change the value if we use var
+    val name: String = "Jose"
+    name = "Dimas"// Error: Val cannot be reassigned
+
+}
