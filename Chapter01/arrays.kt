33fa7e326027a1f482df0a01f8d39c8ed29f6ea8
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-07-04 00:40:39 -0500

    Arrays example

---
+++ Chapter01/arrays.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    //Define an Array
+    val days = arrayOf("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+    // empty Array
+    val emptyDays = emptyArray<String>()
+
+}
