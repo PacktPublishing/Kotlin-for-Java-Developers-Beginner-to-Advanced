616c83b7b8bda7fbb0b74aa32e4fece6ba374d71
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-07-23 01:20:19 -0500

    Main File
    This is the Java code to show how we can call Kotlin code, we are calling MyKFile.kt

---
+++ Chapter02/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    //Calling Java Code
+    public static void main(String[] args){
+        MyKFile myKFile = new MyKFile();
+        myKFile.setName("My  Kotlin file");
+        myKFile.hiKotlin();
+        System.out.println("Hello world Java!");
+    }
+}
