ea504642fced62d1fcdb39b4cd23164267553f7d
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-07-23 01:19:40 -0500

    Add MyJFile
    This code is the Java Example code to that we are using from the Kotlin file helloworld.kt

---
+++ Chapter02/MyJFile.java
@@ -0,0 +1,25 @@
+public class MyJFile {
+
+    public void hiJava(){
+        System.out.println("Hello World Java!");
+        //User variables
+    }
+
+    //Using variables:
+    /*
+    public void hiJava(String name){
+        System.out.println("Hello World Java!" + name);
+    }
+     */
+
+    //Using SetName
+    /*
+    fun main(args: Array<String>){
+
+        println("Hello world Kotlin!")
+        val javaGreeting = MyJFile()
+        javaGreeting.name = "jose"
+        javaGreeting.hiJava()
+    }
+     */
+}
