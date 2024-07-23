90571466b6380cebe4aa3555795cd634f9787c32
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-07-23 01:18:53 -0500

    Hello world
    We have the first hello world and the changes to use Java Code from MyJFile.Java

---
+++ Chapter02/helloworld.kt
@@ -0,0 +1,43 @@
+fun main(args: Array<String>) {
+    println("Hello world!")
+
+    //Use Java code:
+    /*
+    fun main(args: Array<String>){
+        println("Hello world Kotlin!")
+        val javaGreeting = MyJFile()
+        javaGreeting.hiJava()
+    }
+     */
+
+
+    //Passing variables
+    /*
+    fun main(args: Array<String>){
+        println("Hello world Kotlin!")
+        val javaGreeting = MyJFile()
+        javaGreeting.hiJava("jose")
+    }
+     */
+
+
+
+    //Adding Set and Get
+    /*
+    public class MyJFile {
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void hiJava(){
+        System.out.println("Hello World Java!" + this.name);
+    }
+
+     */
+}
