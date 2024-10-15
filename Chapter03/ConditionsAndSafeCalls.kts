bc6d7387f7995b6e4230fefe7e6edf2e234d39a7
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-10-15 11:23:10 -0500

    Conditions and Safe Calls

---
+++ Chapter03/ConditionsAndSafeCalls.kts
@@ -0,0 +1,26 @@
+fun main(args: Array<String> ){
+    var name: String? = "Jose"
+    println(name)
+    println(name.length)
+    //println(name?.length)
+    name= null
+
+    //Condition with String
+    /*
+        var name: String? = "Jose"
+        if (name != null)
+            println(name.length)
+     */
+
+    //Condition with null
+    /*
+    var name: String? = null
+    if (name != null) {
+        println(name.length)
+    }else{
+        println("Name is null")
+    }
+
+     */
+
+}
