598489466e26cbc556a775a545059dffda614191
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-10-15 11:22:28 -0500

    What is Null in Kotlin

---
+++ Chapter03/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    var name: String = "Jose"
+    //able to use null
+    //var name: String? = "Jose"
+    println(name)
+    println(name.length)
+
+    //Non-nullable types:
+    var name: String = "Jose"
+    var age: Int = 10
+    var height: Double = 172.5
+    var student: Boolean = true
+
+    //nullable types
+    /*
+    var name: String? = null
+    var age: Int? = null
+    var height: Double? = null
+    var student: Boolean? = null
+    */
+     
+
+}
