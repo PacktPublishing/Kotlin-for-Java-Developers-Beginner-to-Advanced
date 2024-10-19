20ae6bbf476a7438ef0845fff1b843346ebdbdbe
José Dimas Luján Castillo <josedlujan@hotmail.com>
2024-10-18 19:02:35 -0500

    Kotlin function extension in Java

---
+++ Chapter04/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        String name = "Noemi";
+        char secondChart = MyExtensionKt.second(name);
+        System.out.println(secondChart);
+
+
+    }
+}
