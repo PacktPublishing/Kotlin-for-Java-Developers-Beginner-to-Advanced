07dc4035e67f22b43174d9a0615136966a0db659
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-01-18 17:31:57 -0500

    Create Container.java

---
+++ Chapter06/Container.java
@@ -0,0 +1,16 @@
+public class Container {
+    private String content;
+
+    public Container(String content) {
+        this.content = content;
+    }
+
+    public String getContent(){
+        return  content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+} 
