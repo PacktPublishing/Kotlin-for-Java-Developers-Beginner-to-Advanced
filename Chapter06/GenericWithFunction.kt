1e12277811505f49674192675982820077a9ee74
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-01-18 20:29:35 -0500

    Reified types

--- Chapter06/GenericWithFunction.kt
+++ Chapter06/GenericWithFunction.kt
@@ -1,10 +1,23 @@
 fun <T> printItem(item: T) {
     println(item)
 }
+/*Restrictions on type parameters
+fun <T:Number> printItem(item: T) {
+    println(item)
+}*/
 
+/*Reified types
+inline fun <reified T> printItem(item: T) where T: Number, T:Comparable<T>{
+    when(T::class){
+        Int::class -> println("Integer")
+        Float::class -> println("Float")
+	else -> println("Unknow")
+    }
+}
+ */
 fun main(){
     printItem("Viena")
     printItem(50)
     printItem(true)
 
-} 
+}
