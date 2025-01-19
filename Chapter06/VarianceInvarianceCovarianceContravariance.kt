f4aea1b174ef92f8381a629c51d7d30b5db92313
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-01-18 20:39:47 -0500

    Create Variance, Covariance, Contravariance

---
+++ Chapter06/VarianceInvarianceCovarianceContravariance.kt
@@ -0,0 +1,54 @@
+//Invariance
+open class Figure
+class Triangle : Figure()
+class Circle: Figure()
+class Square: Figure()
+
+class Container<T>(var content: T)
+
+fun main(){
+    val triangleContainer = Container(Triangle())
+    val circleContainer = Container(Circle())
+    val squareContainer = Container(Square())
+    val figureContainer: Container<Figure> = Container(Figure())
+    val figureContainter1: Container<Figure> = triangleContainer
+
+}
+
+/*Covariance(out)
+open class Figure
+class Triangle : Figure()
+class Circle: Figure()
+class Square: Figure()
+
+class Container<out T>(val content: T)
+
+fun main(){
+     val containerTriangle:Container<Triangle> = Container(Triangle())
+     val containerFigure: Container<Figure> = containerTriangle
+ }
+ */
+
+
+/*Contravariance
+open class Figure
+class Triangle : Figure()
+class Circle: Figure()
+class Square: Figure()
+
+class Container<in T> {
+    fun setContent(content:T){
+        println(content)
+    }
+}
+
+fun main(){
+    val figureContainer: Container<Figure> = Container()
+    val triangleConsumer: Container<Triangle> = figureContainer
+
+    triangleConsumer.setContent(Triangle())
+    figureContainer.setContent(Square())
+
+}
+
+ */
