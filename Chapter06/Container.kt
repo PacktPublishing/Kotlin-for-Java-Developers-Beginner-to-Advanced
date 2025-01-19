a78dc68c27203db76a4c4cf3a56fdb7dba6f717f
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-01-18 20:02:04 -0500

    Use Kotlin Container

--- Chapter06/Container.kt
+++ Chapter06/Container.kt
@@ -1,1 +1,11 @@
-class Container<T>(var content: T) 
+class Container<T>(var content: T)
+
+
+fun main(){
+    val myAContainer = Container("jose")
+    val myBContainer = Container(9999)
+
+    println(myAContainer.content)
+    println(myBContainer.content)
+
+} 
