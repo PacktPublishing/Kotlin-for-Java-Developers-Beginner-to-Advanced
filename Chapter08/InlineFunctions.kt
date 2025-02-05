ca7303381d29c9f8316d694009904949cb0e2acb
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-02-04 19:49:13 -0500

    Second section snippets code - Chapter 8

---
+++ Chapter08/InlineFunctions.kt
@@ -0,0 +1,12 @@
+//Section 02
+
+/* 1st Example
+inline fun operate(a: Int, b:Int, operation: (Int, Int) -> Int): Int{
+
+return operation(a,b)
+
+}
+
+val sum = operate(10,1) { x, y -> x + y }
+val product = operate(10,1){ x, y -> x *y }
+ */
