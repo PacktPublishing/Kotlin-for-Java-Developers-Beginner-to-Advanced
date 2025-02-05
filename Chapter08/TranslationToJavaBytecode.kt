fe0c340f132bf3db8a9279b7f2fba27b8c0d5b70
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-02-04 19:49:59 -0500

    Fourth Section Snippet Code: Chapter 8

---
+++ Chapter08/TranslationToJavaBytecode.kt
@@ -0,0 +1,22 @@
+// 1st code
+    fun main(){
+    println("Hello world")
+}
+
+
+/*
+public final class MainKt {
+}
+ */
+
+/* 3
+data class Book(
+
+    val title: String,
+
+    val author: String,
+
+    val isbn: String,
+
+)
+ */
