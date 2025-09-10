d1d2ed159bcd2dace5aec86ec95477dfcf8ea3fa
José Dimas Luján Castillo <josedlujan@hotmail.com>
2025-09-09 22:12:12 -0500

    Add SecondCharacterExample.java with main method

---
+++ Chapter04/SecondCharacterExample.java
@@ -0,0 +1,10 @@
+public class Main { 
+    public static void main(String[] args){ 
+ 
+        String name = "Anna"; 
+        char secondChart = MyExtensionKt.isPalindrome(name); 
+        System.out.println(secondChart); 
+ 
+ 
+    } 
+} 
